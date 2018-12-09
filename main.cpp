7ef4e281db2778425afeedf604dd0fab841f727a
mzainiulilabshor <45529539+mzainiulilabshor@users.noreply.github.com>
2018-12-09 14:43:05 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+ int main()
+{
+    for ( int i=1 ; i<1000 ; i++){
+
+     cout << i <<endl;
+        if (i==10) break;}
+    getch();
+    return 0;
+
+}
